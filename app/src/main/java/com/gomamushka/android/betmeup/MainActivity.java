3e38cc6073e57a0bdc1e717aaece4dd43710741c
Fedor Ard <fed.ushel@gmail.com>
2016-11-30 17:38:35 +0300

    Добавлены новые классы (игроки, аккаунт и т.д.) юзерпики и их случайный выбор и назначение игрокам отображение юзерпиков на сцене игры (тестовая версия)

--- app/src/main/java/com/gomamushka/android/betmeup/MainActivity.java
+++ app/src/main/java/com/gomamushka/android/betmeup/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gomamushka.android.betmeup;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,34 +12,34 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private final String LOG_TAG = "FED";
-    private Integer playerCount;
+    private Integer playersCount;
     private TextView tvPlayers;
     private  Resources res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //ActionBar отключен в Values/style.xml через тему
-        setContentView(R.layout.main_layout);
+         setContentView(R.layout.main_layout);
         tvPlayers = (TextView) findViewById(R.id.tvPlayers);
-        playerCount = 3;
+        playersCount = 3;
         //Через ресурсы чтобы можно было локализовать строку
         res = getResources();
-        String text = res.getString(R.string.players, playerCount);
+        String text = res.getString(R.string.players, playersCount);
         updateTextView(tvPlayers, text);
     }
 
-    private void NewGame() {
-
+    public void newGame(View v) {
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra ("playersCount", playersCount);
+        startActivity(intent);
     }
     public void increasePlayersCount(View view) {
-        playerCount++;
-        String text = res.getString(R.string.players, playerCount);
+        playersCount++;
+        String text = res.getString(R.string.players, playersCount);
         updateTextView(tvPlayers, text);
     }
     public void reducePlayersCount(View view) {
-        playerCount--;
-        Log.d(LOG_TAG, "Игроков:" + playerCount);
-        String text = res.getString(R.string.players, playerCount);
+        playersCount--;
+        String text = res.getString(R.string.players, playersCount);
         updateTextView(tvPlayers, text);
 
     }
